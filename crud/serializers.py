211f6616b1e8fafceb66652f0ecb153f30dc6b30
Muhammad Shafaullah <72027876+MuhammadShafaullah@users.noreply.github.com>
2025-03-20 16:39:54 +0000

    updated
    updated

--- crud/serializers.py
+++ crud/serializers.py
@@ -7,20 +7,8 @@ from rest_framework.renderers import JSONRenderer
 from .models import Student
 
 
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.roll = validated_data.get('roll', instance.roll)
-        instance.city = validated_data.get('city', instance.city)
-        instance.save()
-        return instance
-    
-    def delete(self, instance):
-        instance.delete()
-        return {'msg': 'Student deleted successfully'}
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['name', 'roll', 'city']
     
